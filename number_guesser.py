ab4ea5078a5e123d303ca6a0effc0a8dbeae7461
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-10-29 16:34:59 +0000

    number guesser

---
+++ number_guesser.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# Created by: Teddy Sannan
+# Created on: October 2019
+# This lets the user guess a number
+
+import random
+
+
+def main():
+    while True:
+        # random number
+        number = random.randint(0, 9)
+
+        # input
+        guess_as_string = input("Guess my number between 0 and 9: ")
+
+        # process & output
+        try:
+            # converts number as string to integer
+            guess_as_number = int(guess_as_string)
+
+            # process
+            if guess_as_number == number:
+                # output
+                print()
+                print("Correct! My number was", number)
+
+            # process
+            elif guess_as_number != number:
+                # output
+                print()
+                print("Sorry, thats not it")
+                print("Keep trying")
+                print()
+                continue
+
+        except ValueError:
+            print()
+            print("This is not a integer")
+            print()
+            continue
+
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
